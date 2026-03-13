c1226903f5367800a8160a18ac288f489dd95676
Olof Andersson <olof.andersson@gmail.com>
2026-03-13 13:28:37 +0100

    lower minSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mygame1"
-        minSdk = 36
+        minSdk = 35
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
